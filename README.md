0d6aedfd53ea7c9a0d66b15e17112125a4ab397c
Erik <kazumafan46@gmail.com>
2026-06-21 23:18:35 -0400

    Harness the power of Jimp

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A technical pet-project attempting to recreate systems necessary for a 'workable' internet while minimizing required bandwidth.
 
+```
+node image/encode.js [image file in] [image file out, default: ./output.png]
+```
+
 ## References
 
 https://commons.wikimedia.org/wiki/File:RGB_6bits_palette_sample_image.png
---
+++ image/encode.js
@@ -0,0 +1,42 @@
+const { createJimp } = require("@jimp/core");
+
+const Jimp = createJimp({
+  formats: [
+    require("@jimp/js-png").default,
+    require("@jimp/js-jpeg").default
+  ],
+  plugins: [
+    require("@jimp/plugin-resize").methods,
+    require("@jimp/plugin-color").methods
+  ]
+});
+
+// format works like this: pixels are stored (and subsequently drawn) interspaced
+// consider a 4x4 pixel image, this is how it would be loaded
+// AACC ABCD ABCD ABCD
+// AACC ABCD EBGD EFGH
+// IIKK IJKL IJKL IJKL
+// IIKK IJKL MJOL MNOP
+// I might consider breaking each of these above pixels into groups of 2x2 pixels themselves for even faster loading of a usable image, but whatever
+// this allows you to, say, only have 25% of the image data and still see a "full" image (that gets refined as you get more information)
+// the decode script will have an option to stop a percentage of the way through decoding to see what it looks like
+
+(async () => {
+
+    const path = process.argv[2];
+
+    try {
+        const image = await Jimp.read(path);
+
+        await image
+            .resize({ w: 256 })
+            .contrast(0.2) // adds 20%
+            .posterize(4); // 6 bit
+
+        await image.write("output.png");
+
+    } catch (error) {
+        console.error("An error occurred while processing the image:", error);
+    }
+
+})();
